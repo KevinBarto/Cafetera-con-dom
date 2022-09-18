4873eccbbc93e287f1a5b6c54b8407a46b896d3f
kevin <kevin.bartoliche@hotmail.com>
2022-09-18 10:45:34 -0300

    Devolucion de lauta

--- app.js
+++ app.js
@@ -73,3 +73,44 @@ finalizar.onclick = function(){
 //     let negativo =document.createElement("div")
 //     negativo.innerHTML=`*ruido a mate*`
 // }
+
+
+// Buenas Kevin! Cómo estás?
+
+// Genial que tengas una mentora por ahi :D jaja
+
+// El onclick funciona en mobile igual, lo que es considerado mala práctica, es usarlo en el html como tenes en tus button. Lo ideal es sumarle los eventos en js, con addEventListener, o .onclick ..
+
+// Para no repetir mucho el selector lo que podes hacer es, aprovechando que ya tenes tus cafes en objetos, generar los botones a partir de eso (comentando los que tenes en el html) 
+
+// Basicamente como hiciste con el resúmen de la compra al final:
+
+// // guardo los objetos en un array
+// const cafes = [cafe1, cafe2, cafe3, cafe4];
+
+// const botonSi = document.querySelector("#btnSi");
+// botonSi.onclick = () => generarDOM()
+
+// function generarDOM() {
+//     document.querySelector("#selectorCafe").style.opacity = 1;
+//     for (let cafe of cafes) {
+// //por cada café creo un elemento nuevo <article>
+//         const cafeDOM = document.createElement('article')
+// //muestro su nombre y le sume el precio
+//         cafeDOM.innerHTML = `<h4>${cafe.nombre}</h4>
+//         <span>${cafe.precio}</span>`
+// //creo el botón
+//         const cafeButton = document.createElement("button")
+//         cafeButton.innerText = 'agregar'
+// //llama a la función pasandole el cafe correspondiente
+//         cafeButton.onclick = () => agregar(cafe)
+// //finalmente agrego todo al dom
+//         document.querySelector("#selectorCafe").append(cafeDOM, cafeButton)
+//     }
+// }
+// Así evitas las malas prácticas, además de reducir el código. Cualquier duda, consultame! 
+
+// Aprobadas las 2 entregas, ya que cumplen ambos requisitos, 
+
+// Abrazo!
+
